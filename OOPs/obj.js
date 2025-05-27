bcf724047c5092b2aea2695d018847de9b112cf0
Mohd Safee <2k23.cs2313689@gmail.com>
2025-05-27 09:00:11 +0000

    little OOPs basics

---
+++ OOPs/obj.js
@@ -0,0 +1,14 @@
+var students = {}
+
+students["name"] = "Abhiseen"
+students["age"]=57
+
+console.log(students);
+
+let x=5;
+let y=10;
+if(x < y){
+    students = {greeting : "hillo boy"};
+}
+
+console.log(students);
---
+++ OOPs/objectOfobjects.js
@@ -0,0 +1,7 @@
+const myHonda = {
+    color : "red",
+    wheels: 4,
+    engine: {cylinders: 4, size: 2.2},
+};
+
+console.log(myHonda.engine.cylinders);
